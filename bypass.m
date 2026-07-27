53a0b8a69d2e45999bb31dce8c5473a9d18b2ac8
shadow-1337cmds <hasnew.business@gmail.com>
2026-07-27 22:24:36 +0200

    Update bypass.m

--- bypass.m
+++ bypass.m
@@ -2,20 +2,17 @@
 #import <sys/sysctl.h>
 #import <unistd.h>
 
-// Перехват getppid (обман проверки родительского процесса)
+// Обман проверки родительского процесса
 pid_t fake_getppid(void) {
-    // Возвращаем 1 (launchd), как у обычного легитимного приложения
     return 1; 
 }
 
-// Перехват sysctl (обман детекта эмуляторов и отладки)
-int fake_sysctl(int *name, u_int namelen, void *oldp, size_size_t *oldlenp, void *newp, size_t newlen) {
-    // Если игра ищет признаки отладки (KERN_PROC_PID / P_TRACED)
+// Обман детекта отладки через sysctl
+int fake_sysctl(int *name, u_int namelen, void *oldp, size_t *oldlenp, void *newp, size_t newlen) {
     if (namelen >= 4 && name[0] == CTL_KERN && name[1] == KERN_PROC && name[2] == KERN_PROC_PID) {
         int res = sysctl(name, namelen, oldp, oldlenp, newp, newlen);
         if (res == 0 && oldp && *oldlenp >= sizeof(struct kinfo_proc)) {
             struct kinfo_proc *kp = (struct kinfo_proc *)oldp;
-            // Сбрасываем флаг отладки в ноль
             kp->kp_proc.p_flag &= ~P_TRACED;
         }
         return res;
@@ -24,6 +21,5 @@ int fake_sysctl(int *name, u_int namelen, void *oldp, size_size_t *oldlenp, void
 }
 
 __attribute__((constructor)) static void init() {
-    NSLog(@"[GrandBypass] Твик успешно загружен! Отключаем проверки...");
-    // Здесь Sideloadly или утилита инжекции свяжет эти функции с оригинальными вызовами
+    NSLog(@"[GrandBypass] Твик загружен. Проверки sysctl и getppid отключены.");
 }
